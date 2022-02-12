db7563130a74ab282392a754aae18dcfe582681e
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-12 11:54:46 -0500

    add dummy data

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const DUMMY_DATA = [
+    { id: 'd1', value: 10, region: 'USA'},
+    { id: 'd2', value: 20, region: 'India'},
+    { id: 'd3', value: 30, region: 'China'},
+    { id: 'd4', value: 40, region: 'Switzerland'},
+];
+
+
 d3.select('div')
   .selectAll('p')
   // bind data to paragraphs
